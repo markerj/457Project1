be96a5117881c021b5036b4faf017f8b6d157cae
tylerpaquet <paquett@mail.gvsu.edu>
2017-09-25 16:22:41 -0400

    Fixed formatting

--- Client2.java
+++ Client2.java
@@ -3,201 +3,156 @@ import java.nio.*;
 import java.io.ByteArrayOutputStream;
 import java.net.*;
 import java.nio.channels.*;
-public class Client2
-{
 
-	public static void main (String [] args ) throws IOException
-	{
-    
-		boolean exit = false;
- 		String fileName = null;
-		String sentence;
-		String serverSentence;
-    	int bytesRead;
-    	int current = 0;
+public class Client2 {
 
-		InputStream is = null;
-    	DataOutputStream outToServer = null;
-    	FileOutputStream fos = null;
-    	BufferedOutputStream bos = null;
-    	Socket sock = null;
-		BufferedReader inFromUser = null;
-    	BufferedReader inFromServer = null;	
-    	try
-    	{
-			int port = getPort();
-			String ip = getIP();
-			sock = new Socket(ip, port);
-			System.out.println("Connection to server: " + sock);
-			while (!exit)
-			{
-				outToServer = new DataOutputStream(sock.getOutputStream());
-				inFromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
-				inFromUser = new BufferedReader(new InputStreamReader(System.in));
-				System.out.println("Commands: exit, send <filename>, listFiles");
-				sentence = inFromUser.readLine();
-				if (sentence.equals("exit"))
-				{
-					exit = true;
-				}	
-				outToServer.writeBytes(sentence + '\n');
-				serverSentence = inFromServer.readLine();
-				System.out.println("Server Respone: " + serverSentence);
-				if (sentence.length() > 4)
-				{
-					if (sentence.substring(0, 4).toLowerCase().equals("send"))
-					{
-						String recieve = inFromServer.readLine();
-						if(recieve.equals("File does not exist"))
-						{
-							System.out.println("File does not exist");
-							sock = new Socket(ip, port);
-						}
-						else
-						{
-							fileName = sentence.substring(5,sentence.length());
-      						byte [] byteArray  = new byte [6500000];
-      						is = sock.getInputStream();
-							fos = new FileOutputStream("downloaded." + sentence.substring((sentence.length()-3),sentence.length()));
-      
-      						bos = new BufferedOutputStream(fos);
-      						bytesRead = is.read(byteArray,0,byteArray.length);
-      						current = bytesRead;
-	
-							while(bytesRead > -1)
-							{
-	 							bytesRead = is.read(byteArray, current, (byteArray.length-current));
-          						if(bytesRead >= 0)
-          						{
-	   								current += bytesRead;
-            					}	
-        					}	
-      						bos.write(byteArray, 0 , current);
-      						bos.flush();
-      						System.out.println("File " + fileName + " downloaded");
-							sock = new Socket(ip, port);
-						}
-						
-    				}
-    				else if(sentence.substring(0, 4).toLowerCase().equals("list"))
-    				{
-    					String list = inFromServer.readLine();
-    					String[] splitList = list.split(" ");
-    					System.out.println("---------------------");
-    					for(int i = 0; i < splitList.length; i++)
-    					{
-    						System.out.println(splitList[i]);
-    					}
-    					System.out.println("---------------------");
-    					sock = new Socket(ip, port);
-    				}
-    			}
-    			else
-    			{
-    				System.out.println("That is not a known command");
-    				sock = new Socket(ip, port);
-    			}
-    		}
-		}
-    	finally
-    	{
-			if (fos != null)
-				fos.close();
-			if (bos != null)
-				bos.close();
-			if (sock != null)
-				sock.close();
-		}
-	}
+    public static void main(String[] args) throws IOException {
+
+        boolean exit = false;
+        String fileName = null;
+        String sentence;
+        String serverSentence;
+        int bytesRead;
+        int current = 0;
+
+        InputStream is = null;
+        DataOutputStream outToServer = null;
+        FileOutputStream fos = null;
+        BufferedOutputStream bos = null;
+        Socket sock = null;
+        BufferedReader inFromUser = null;
+        BufferedReader inFromServer = null;
+        try {
+            int port = getPort();
+            String ip = getIP();
+            sock = new Socket(ip, port);
+            System.out.println("Connection to server: " + sock);
+            while (!exit) {
+                outToServer = new DataOutputStream(sock.getOutputStream());
+                inFromServer = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                inFromUser = new BufferedReader(new InputStreamReader(System.in));
+                System.out.println("Commands: exit, send <filename>, listFiles");
+                sentence = inFromUser.readLine();
+                if (sentence.equals("exit")) {
+                    exit = true;
+                }
+                outToServer.writeBytes(sentence + '\n');
+                serverSentence = inFromServer.readLine();
+                System.out.println("Server Respone: " + serverSentence);
+                if (sentence.length() > 4) {
+                    if (sentence.substring(0, 4).toLowerCase().equals("send")) {
+                        String recieve = inFromServer.readLine();
+                        if (recieve.equals("File does not exist")) {
+                            System.out.println("File does not exist");
+                            sock = new Socket(ip, port);
+                        } else {
+                            fileName = sentence.substring(5, sentence.length());
+                            byte[] byteArray = new byte[6500000];
+                            is = sock.getInputStream();
+                            fos = new FileOutputStream("downloaded." + sentence.substring((sentence.length() - 3), sentence.length()));
+
+                            bos = new BufferedOutputStream(fos);
+                            bytesRead = is.read(byteArray, 0, byteArray.length);
+                            current = bytesRead;
+
+                            while (bytesRead > -1) {
+                                bytesRead = is.read(byteArray, current, (byteArray.length - current));
+                                if (bytesRead >= 0) {
+                                    current += bytesRead;
+                                }
+                            }
+                            bos.write(byteArray, 0, current);
+                            bos.flush();
+                            System.out.println("File " + fileName + " downloaded");
+                            sock = new Socket(ip, port);
+                        }
+
+                    } else if (sentence.substring(0, 4).toLowerCase().equals("list")) {
+                        String list = inFromServer.readLine();
+                        String[] splitList = list.split(" ");
+                        System.out.println("---------------------");
+                        for (int i = 0; i < splitList.length; i++) {
+                            System.out.println(splitList[i]);
+                        }
+                        System.out.println("---------------------");
+                        sock = new Socket(ip, port);
+                    }
+                } else {
+                    System.out.println("That is not a known command");
+                    sock = new Socket(ip, port);
+                }
+            }
+        } finally {
+            if (fos != null)
+                fos.close();
+            if (bos != null)
+                bos.close();
+            if (sock != null)
+                sock.close();
+        }
+    }
 
 
-	private static int getPort()
-	{
-		int port = 0;
-		boolean valid = false;
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter a port number: ");
+    private static int getPort() {
+        int port = 0;
+        boolean valid = false;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a port number: ");
 
-		while (!valid)
-		{
-			try
-			{
-				port = Integer.parseInt(input.readLine());
-				if (port > 1024 && port < 65536)
-				{
-					valid = true;
-				}
-				else
-				{
-					System.out.println("Invalid Port. \nEnter a port number: ");
-				}
-			}
-			catch (NumberFormatException e)
-			{
-				System.out.println("Invalid Port. \nEnter a port number: ");
-			}
-			catch (IOException e)
-			{
-				System.out.println("Invalid Port. \nEnter a port number: ");
-			}
-		}
-		System.out.println("connecting to port: " + port);
+        while (!valid) {
+            try {
+                port = Integer.parseInt(input.readLine());
+                if (port > 1024 && port < 65536) {
+                    valid = true;
+                } else {
+                    System.out.println("Invalid Port. \nEnter a port number: ");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Port. \nEnter a port number: ");
+            } catch (IOException e) {
+                System.out.println("Invalid Port. \nEnter a port number: ");
+            }
+        }
+        System.out.println("connecting to port: " + port);
 
-		return port;
-	}
+        return port;
+    }
 
-	private static String getIP()
-	{
-		String ipAddr = "";
-		boolean validIPAddr = false;
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter an IP address: ");
-		while (validIPAddr== false)
-		{
-			try
-			{
-				ipAddr = input.readLine();
-	        	String[] pieces = ipAddr.split("\\.");
-				
-				for(String s: pieces)
-				{
-					int i = Integer.parseInt(s);
-					if((i < 0) || i > 255){
-						System.out.println("IP address parts cannot be smaller than 0 or larger than 255. \nEnter an IP Address: ");
-						break;
-					}
-				}
-				
-				if(ipAddr == null || ipAddr.isEmpty())
-				{
-					System.out.println("IP address is empty. \nEnter an IP Address: ");
-				}
-				else if(pieces.length != 4)
-				{
-					System.out.println("IP address does not contain correct number of parts. \nEnter an IP Address: ");
-				}
-				
-				else if(ipAddr.endsWith("."))
-				{
-					System.out.println("IP addresses cannot end with a period. \nEnter an IP Address: ");
-				}
-				
-				else
-				{
-					validIPAddr = true;
-				}
+    private static String getIP() {
+        String ipAddr = "";
+        boolean validIPAddr = false;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter an IP address: ");
+        while (validIPAddr == false) {
+            try {
+                ipAddr = input.readLine();
+                String[] pieces = ipAddr.split("\\.");
+
+                for (String s : pieces) {
+                    int i = Integer.parseInt(s);
+                    if ((i < 0) || i > 255) {
+                        System.out.println("IP address parts cannot be smaller than 0 or larger than 255. \nEnter an IP Address: ");
+                        break;
+                    }
+                }
+
+                if (ipAddr == null || ipAddr.isEmpty()) {
+                    System.out.println("IP address is empty. \nEnter an IP Address: ");
+                } else if (pieces.length != 4) {
+                    System.out.println("IP address does not contain correct number of parts. \nEnter an IP Address: ");
+                } else if (ipAddr.endsWith(".")) {
+                    System.out.println("IP addresses cannot end with a period. \nEnter an IP Address: ");
+                } else {
+                    validIPAddr = true;
+                }
 
 
-			}
-			catch (NumberFormatException e)
-			{
-				System.out.println("Invalid IP. \nEnter an IP address: ");
-			}
-			catch (IOException e)
-			{
-				System.out.println("Invalid IP. \nEnter an IP address: ");
-			}
-		}
-		return ipAddr;
-	}
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid IP. \nEnter an IP address: ");
+            } catch (IOException e) {
+                System.out.println("Invalid IP. \nEnter an IP address: ");
+            }
+        }
+        return ipAddr;
+    }
 } 
--- Server2.java
+++ Server2.java
@@ -3,173 +3,135 @@ import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
 
-public class Server2
-{
+public class Server2 {
 
- 	public static void main (String [] args ) throws IOException
- 	{
-     
+    public static void main(String[] args) throws IOException {
+
         boolean validFile = false;
-		boolean validPortNum = false;
+        boolean validPortNum = false;
         int portNum = 0;
         String ipAddr = null;
-       	ServerSocket servsock = null;
-       	Socket sock = null;
-	
-		try
-		{
-			portNum = getPort();	
- 			servsock = new ServerSocket(portNum);
-			System.out.println("Waiting for connection");
-      		while (true)
-      		{
-        		try
-        		{		
-          			sock = servsock.accept();
-					new ServerThread(sock).start();
-        		}
-				catch(IOException e)
-				{
-				}
-        	}	
-      	}
-     	finally
-     	{
-			if (servsock != null)
-				servsock.close();
-		}
-    	
- 	}
- 	
- 	private static int getPort()
- 	{
-		int port = 0;
-		boolean valid = false;
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println("Enter a port number: ");
+        ServerSocket servsock = null;
+        Socket sock = null;
 
-		while (!valid)
-		{
-			try
-			{
-				port = Integer.parseInt(input.readLine());
-				if (port > 1024 && port < 65536)
-				{
-					valid = true;
-				}
-				else
-				{
-					System.out.println("Invalid Port. \nEnter a port number: ");
-				}
-			}
-			catch (NumberFormatException e)
-			{
-				System.out.println("Invalid Port. \nEnter a port number: ");
-			}
-			catch (IOException e)
-			{
-				System.out.println("Invalid Port. \nEnter a port number: ");
-			}
-		}
-		System.out.println("connecting to port: " + port);
-		return port;
-	}
+        try {
+            portNum = getPort();
+            servsock = new ServerSocket(portNum);
+            System.out.println("Waiting for connection");
+            while (true) {
+                try {
+                    sock = servsock.accept();
+                    new ServerThread(sock).start();
+                } catch (IOException e) {
+                }
+            }
+        } finally {
+            if (servsock != null)
+                servsock.close();
+        }
+
+    }
+
+    private static int getPort() {
+        int port = 0;
+        boolean valid = false;
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a port number: ");
+
+        while (!valid) {
+            try {
+                port = Integer.parseInt(input.readLine());
+                if (port > 1024 && port < 65536) {
+                    valid = true;
+                } else {
+                    System.out.println("Invalid Port. \nEnter a port number: ");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid Port. \nEnter a port number: ");
+            } catch (IOException e) {
+                System.out.println("Invalid Port. \nEnter a port number: ");
+            }
+        }
+        System.out.println("connecting to port: " + port);
+        return port;
+    }
 
 }
 
-class ServerThread extends Thread
-{
+class ServerThread extends Thread {
 
     Socket sock = null;
-    
-    public ServerThread(Socket clientSocket)
-    {
-       this.sock = clientSocket;
+
+    public ServerThread(Socket clientSocket) {
+        this.sock = clientSocket;
     }
 
-    public void run()
-    {
-		System.out.println("Connection established: " + sock);
-		String clientSentence;
+    public void run() {
+        System.out.println("Connection established: " + sock);
+        String clientSentence;
         BufferedReader inFromClient = null;
         FileInputStream fis = null;
         BufferedInputStream bis = null;
         OutputStream os = null;
-		try
-		{
-			try
-			{
-                  
-				inFromClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));  
-				DataOutputStream outToClient = new DataOutputStream(sock.getOutputStream());
-         		clientSentence = inFromClient.readLine();
-				System.out.println("Client said: " + clientSentence);
-				outToClient.writeBytes(clientSentence + '\n');
+        try {
+            try {
 
-				String fileToSend = clientSentence.substring(5,clientSentence.length());
-				
-				if (clientSentence.length() > 4)
-				{
-					if (clientSentence.substring(0, 4).toLowerCase().equals("send"))
-					{					
-           				File myFile = new File (fileToSend);
+                inFromClient = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(sock.getOutputStream());
+                clientSentence = inFromClient.readLine();
+                System.out.println("Client said: " + clientSentence);
+                outToClient.writeBytes(clientSentence + '\n');
+
+                String fileToSend = clientSentence.substring(5, clientSentence.length());
+
+                if (clientSentence.length() > 4) {
+                    if (clientSentence.substring(0, 4).toLowerCase().equals("send")) {
+                        File myFile = new File(fileToSend);
                         // check if myFile.exists() 
-						if(!myFile.exists() || myFile.isDirectory())
-						{
-							System.out.println("File does not exist");
-							//outToClient.writeBytes("File does not exist");
-							outToClient.writeBytes("File does not exist");
-							outToClient.flush();
-							
-						}
-                        else
-                        {
-							byte [] byteArray  = new byte [(int)myFile.length()];
-							fis = new FileInputStream(myFile);
-							bis = new BufferedInputStream(fis);
-							bis.read(byteArray,0,byteArray.length);
-							os = sock.getOutputStream();
-							os.write(byteArray,0,byteArray.length);
-							os.flush();
-							System.out.println("File sent");
-						}
-				 
-					}
-					
-					//send list of filenames
-					else if(clientSentence.substring(0, 4).toLowerCase().equals("list"))
-					{
-						String list = "";
-						File dir = new File(".");
-						File[] filesList = dir.getCanonicalFile().listFiles();
-						for(int i = 0; i < filesList.length; i++)
-						{
-							if(filesList[i].isFile())
-							{
-								list += (filesList[i].getName() + " ");
-							}
-						}
-						outToClient.writeBytes(list);
-						outToClient.flush();
-						System.out.println("Sent list of files to Client");
-					}
-				}
-			}
-			finally
-			{					
-				if (bis != null)
-					bis.close();
-				if (os != null)
-					os.close();
-				if (sock != null)
-					sock.close();
-			}
-		}
-		catch(IOException e)
-		{
-		}
-		catch(Exception e)
-		{
-		}
-	}
+                        if (!myFile.exists() || myFile.isDirectory()) {
+                            System.out.println("File does not exist");
+                            //outToClient.writeBytes("File does not exist");
+                            outToClient.writeBytes("File does not exist");
+                            outToClient.flush();
+
+                        } else {
+                            byte[] byteArray = new byte[(int) myFile.length()];
+                            fis = new FileInputStream(myFile);
+                            bis = new BufferedInputStream(fis);
+                            bis.read(byteArray, 0, byteArray.length);
+                            os = sock.getOutputStream();
+                            os.write(byteArray, 0, byteArray.length);
+                            os.flush();
+                            System.out.println("File sent");
+                        }
+
+                    }
+
+                    //send list of filenames
+                    else if (clientSentence.substring(0, 4).toLowerCase().equals("list")) {
+                        String list = "";
+                        File dir = new File(".");
+                        File[] filesList = dir.getCanonicalFile().listFiles();
+                        for (int i = 0; i < filesList.length; i++) {
+                            if (filesList[i].isFile()) {
+                                list += (filesList[i].getName() + " ");
+                            }
+                        }
+                        outToClient.writeBytes(list);
+                        outToClient.flush();
+                        System.out.println("Sent list of files to Client");
+                    }
+                }
+            } finally {
+                if (bis != null)
+                    bis.close();
+                if (os != null)
+                    os.close();
+                if (sock != null)
+                    sock.close();
+            }
+        } catch (IOException e) {
+        } catch (Exception e) {
+        }
+    }
 }   
